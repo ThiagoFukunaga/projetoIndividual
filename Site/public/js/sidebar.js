df02940b90d756760094653edea560d6398425b8
ThiagoFukunaga <thiago.fukunaga@sptech.school>
2025-06-04 13:50:13 -0300

    estlização parcial da dashboard

--- Site/public/js/sidebar.js
+++ Site/public/js/sidebar.js
@@ -20,3 +20,31 @@ function redirecionarDash(){
 function redirecionarInicio(){
     window.location = "./logado.html"
 }
+
+function mudarTema() {
+    const root = document.documentElement;
+
+    if (sessionStorage.getItem("ESTACAO_USUARIO") == "Inverno") {
+        root.style.setProperty('--corSide', '#b2caffba');
+        root.style.setProperty('--corSideSec', '#CCDBD9');
+        root.style.setProperty('--corPrincipal', '#B2CAFF');
+
+    } else if (sessionStorage.getItem("ESTACAO_USUARIO") == "Outono") {
+        root.style.setProperty('--corSide', '#aab5b86d');
+        root.style.setProperty('--corSideSec', '#CCDBD9');
+        root.style.setProperty('--corPrincipal', '#AAB5B8');
+
+
+    } else if (sessionStorage.getItem("ESTACAO_USUARIO") == "Primavera") {
+        root.style.setProperty('--corSide', '#c3b1e674');
+        root.style.setProperty('--corSideSec', '#8F6CF0');
+        root.style.setProperty('--corPrincipal', '#C3B1E6');
+
+    } else if (sessionStorage.getItem("ESTACAO_USUARIO") == "Verão") {
+        
+        root.style.setProperty('--corSide', '#7cb3426f');
+        root.style.setProperty('--corSideSec', '#33691E');
+        root.style.setProperty('--corPrincipal', '#7CB342');
+    }
+
+}
